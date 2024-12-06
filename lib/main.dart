68d6ba673b1d4d5ca7f9942a3b87e32e100d7176
mohan668 <mohans1469@gmail.com>
2024-12-06 21:50:48 +0530

    tried turning blue to green

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomeScreen(), // The landing screen of your app
+      home: const HomeScreen(), // The landing screen of your app
     );
   }
 }
